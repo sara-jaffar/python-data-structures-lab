fc08cf6b83e942f2ffeebe5658260d060bc6ac68
sara-jaffar <sara.alshoula55@email.com>
2025-08-19 10:11:18 +0300

    added to ex 4

--- exercises.py
+++ exercises.py
@@ -57,6 +57,10 @@ def slice_foods():
 # Call the function and print the result
 print('Exercise 3:', slice_foods())
 
+# Exercise 4: Dictionaries and String Formatting
+#
+# Create a dictionary named home_town containing the keys of city, state, and population.
+# Using the home_town dictionary, assign to a variable named home_town_message a string with this format: “I was born in <city>, <state> - population of <population>”
 def hometown_info():
     home_town = {
         "city": "Riyadh",
